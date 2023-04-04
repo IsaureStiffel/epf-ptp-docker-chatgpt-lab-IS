de2f4c8656a01c3151be2e3253a7c3b96fec8ea5
IsaureStiffel <120568787+IsaureStiffel@users.noreply.github.com>
2023-04-04 12:41:28 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -24,15 +24,12 @@ def chatgpt():
 
 @app.route('/generate-code', methods=['POST'])
 def generate_code():
-    language = request.form['language']
-    content = request.form['content']
-    prompt = f"Generate {language} code for the following:\n\n{content}\n\nCode:"
+    args = request.args
+    language = args.get("language")
+    content = args.get("content")
+    message = f"Generate {language} code for the following:\n\n{content}\n\nCode:"
     completion = openai.Completion.create(
-        engine="davinci-codex",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}]
     )
-    return completion.choices[0].text
+    return completion['choices'][0]['message']['content']
